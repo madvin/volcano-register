4223feac397985d58ba6804230e96a2a2d92aac8
madvin <madvin@abv.bg>
2025-04-09 09:04:26 +0300

    initialize the project

---
+++ app/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import handlebars from 'express-handlebars';
+
+const app = express();
+
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+    res.send('It works')
+});
+
+
+app.listen(`${port}`, () => console.log(`Server is listening on http://localhost:${port}`))
