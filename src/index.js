806f68e8af1f8e9b9473c313c32230cffc5692ff
madvin <madvin@abv.bg>
2025-04-09 19:04:26 +0300

    st routes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import routes from './routes,js';
 
 const app = express();
 
 const port = 3000;
 
 app.use('/static', express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+app.use(routes);
 
 
 
